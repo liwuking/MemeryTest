7ad875b670e8d220ec633e87621903d7cd1a87a3
liwuyang <liwuyang@lbesec.com>
2018-04-08 14:49:39 +0800

    Initial Commit

---
+++ MemoryTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemoryTest
+//
+//  Created by lbe on 2018/4/8.
+//  Copyright © 2018年 liwuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
